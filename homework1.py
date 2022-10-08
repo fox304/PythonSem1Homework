0019937f9ae3e7d89c4be72ca7cfad4222e80ee8
Maks <nordmax69@gmail.com>
2022-10-09 02:29:35 +0700

    created file with weekdays

---
+++ homework1.py
@@ -0,0 +1,16 @@
+# Напишите программу, которая принимает на вход цифру, 
+# обозначающую день недели, и проверяет, является ли этот день выходным
+
+list_of_weekdays = ["Mondey","Tuesday","Wednesday","Thurthday","Friday","Saturday","Sunday"]
+
+number_of_weekday = input("Введите номер дня недели (от 1 до 7) ")
+
+while len(number_of_weekday) > 1 or not (48 < ord(number_of_weekday) < 56):  # запрет на ввод данных , отличных от цифр 1,2,3,4,5,6,7
+    number_of_weekday = input("Совеную всё-таки ввести цифру от 1 до 7 , иначе день пролетит незаметно ")
+
+if int(number_of_weekday) < 6:
+    yes_or_not = "НЕ"
+else:
+    yes_or_not = ""
+
+print("{0}  {1} является  выходным днём".format(list_of_weekdays[int(number_of_weekday)-1],yes_or_not))
