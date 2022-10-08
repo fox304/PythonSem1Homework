a07bc52ff4bb4c936e26dfbd4b4ebf4e31f9146d
Maks <nordmax69@gmail.com>
2022-10-09 00:40:11 +0700

    created file with predicats

---
+++ homework2.py
@@ -0,0 +1,16 @@
+# Напишите программу для проверки истинности утверждения 
+# ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+# NOT(X OR Y OR Z) = NOT X AND NOT Y AND NOT Z
+x = y = z =list([True,False])
+
+count_False_staytment = 0 # ведёт подсчёт ложных утверждений
+for i in x:
+    for j in y:
+        for k in z:
+            if not(i or j or k) != (not i and not j and not k):
+                count_False_staytment +=1
+                
+print("Утверждение верно при любых значений предикат" \
+      if count_False_staytment == 0 else "Утврждение ложно")
+                                        
+           
